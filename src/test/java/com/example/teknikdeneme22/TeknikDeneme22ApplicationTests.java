f84a6247fb870badc7ef6ed184b04f861918ee94
anilkabar <81707871+anilkabar@users.noreply.github.com>
2024-01-05 13:40:23 +0300

    .
    .

---
+++ src/test/java/com/example/teknikdeneme22/TeknikDeneme22ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.teknikdeneme22;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeknikDeneme22ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
